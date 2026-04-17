97ccf51e81476f60dfd29995416ecf37443d4da3
Thomas Neidhart <thomas.neidhart@eclipse-foundation.org>
2026-04-17 15:08:24 +0200

    add visualizer plugin

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -1,11 +1,12 @@
 import path from 'node:path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 const outRootDir = path.join(__dirname, "dist");
 
 export default defineConfig(() => ({
-    plugins: [react()],
+    plugins: [react(), visualizer() as PluginOption],
     server: {
         host: true,
         port: 3000,
@@ -20,15 +21,16 @@ export default defineConfig(() => ({
     },
     publicDir: 'static',
     build: {
-        target: 'es6',
+        target: 'es2020',
         minify: true,
         sourcemap: true,
         outDir: outRootDir,
         emptyOutDir: true,
         rollupOptions: {
             output: {
-                entryFileNames: 'bundle.js',
-                assetFileNames: 'bundle-[name].css',
+                entryFileNames: 'bundle-[hash].js',
+                assetFileNames: '[name]-[hash][extname]',
+                chunkFileNames: 'chunk-[name]-[hash].js',
             }
         }
     },
